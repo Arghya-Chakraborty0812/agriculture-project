8506bfe74505933dfddac583a9818c664abf0d35
Arghya Chakraborty <arghya0812@gmail.com>
2024-12-19 23:27:57 +0530

    Minor dark-light toggle in aboutus

---
+++ About us/aboutUs.js
@@ -0,0 +1,21 @@
+let btn1 = document.querySelector("#btn-mode");
+let body = document.querySelector("body");
+
+let curr = "light";
+
+btn1.addEventListener("click", () => {
+    if(curr === "light")
+    {
+        curr = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+        
+    }
+    else{
+        curr = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+        
+    }
+
+});
