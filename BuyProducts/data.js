61f4895d3b025b74005bca6fcd5242be0f586e28
pratapVansh <vanshprataps2004@gmail.com>
2025-01-23 21:45:41 +0530

    Added product listing page functionality

--- BuyProducts/data.js
+++ BuyProducts/data.js
@@ -21,4 +21,28 @@ let shopItemData = [{
     name:"Organic Manure",
     price:490,
     img : "../images/organic manuere.webp",
+},
+{
+    id:"item5",
+    name:"Biovita Liquid Fertilizer",
+    price:300,
+    img : "../images/biovita.jpg",
+},
+{
+    id:"item6",
+    name:"Hybrid Tomato Seeds",
+    price:550,
+    img : "../images/tomato.jpg",
+},
+{
+    id:"item7",
+    name:"Hybrid Chilli Seeds",
+    price:450,
+    img : "../images/chilli.jpg",
+},
+{
+    id:"item8",
+    name:"Soil Enhancer",
+    price:200,
+    img : "../images/soil enhancer.jpeg",
 }];
